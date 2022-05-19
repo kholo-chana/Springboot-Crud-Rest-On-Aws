a6ef65e026ee0365f0ae8f08a080c92311e8ec47
kholo-chana <phaswane8@gmail.com>
2022-05-19 15:55:32 +0200

    Initial commit

---
+++ springboot-crud-restful-webservices-master/src/main/java/net/kholofelo/springboot/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package net.kholofelo.springboot.repository;
+
+import net.kholofelo.springboot.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
